8dc9fafa4bfb968eac86562ea93e6d3eb549ebba
alexander.fernandez@skillassure.com <alexander.fernandez@skillassure.com>
2022-06-22 13:06:24 +0530

    JavaAssignment2 comitted

---
+++ JavaAssignment2/src/test/java/com/capgemini/javaAssignment2/JavaAssignment2.java
@@ -0,0 +1,109 @@
+package com.capgemini.javaAssignment2;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
+
+import com.opencsv.CSVReader;
+
+
+public class JavaAssignment2 {
+	public static boolean isNumeric(String strNum) {
+		if (strNum == null) {
+			return false;
+		}
+		try {
+			double d = Double.parseDouble(strNum);
+		} catch (NumberFormatException nfe) {
+			return false;
+		}
+		return true;
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		//Reading 1st csv file
+
+		ArrayList<String> array1 = new ArrayList<String>(); 
+		CSVReader reader1 = null;  
+		reader1 = new CSVReader(new FileReader("./src/test/resources/DataSourceFile1.csv"));    
+
+		String [] nextLine1;  
+		//read one line at a time  
+		while ((nextLine1 = reader1.readNext())!= null)  
+		{  
+			for(int i=0; i<nextLine1.length;i++) {
+				array1.add(nextLine1[i]);
+			}
+		} 
+		System.out.println(array1);
+		//Reading 2nd csv file
+
+		ArrayList<String> array2 = new ArrayList<String>(); 
+		CSVReader reader2 = null;
+		reader2 = new CSVReader(new FileReader("./src/test/resources/DataSourceFile2.csv"));  
+
+		String [] nextLine2;  
+
+		//read one line at a time  
+		while ((nextLine2 = reader2.readNext()) != null)  
+		{  
+			for(int i=0; i<nextLine2.length;i++) {
+				array2.add(nextLine2[i]);
+			}
+		}  
+		System.out.println(array2);
+
+		//Listing the difference
+		ArrayList<String> array3 = new ArrayList<String>(); 
+		for(int i=0; i<array1.size();i++) {
+
+			if(isNumeric(array1.get(i)) && isNumeric(array2.get(i))) {
+				double a = Double.parseDouble(array1.get(i));
+				double b = Double.parseDouble(array2.get(i));
+
+				String result = String.valueOf(a-b);
+
+				if((a-b)==0) {
+					array3.add("NA");
+				}
+				else {
+					array3.add(result);
+				}	
+			}	
+			else { 
+				String difference =StringUtils.difference(array1.get(i),array2.get(i));
+				if(difference.isEmpty()) {
+					array3.add("NA");
+				}
+				else {
+					array3.add(StringUtils.difference(array1.get(i),array2.get(i)));
+				}
+			}	
+		}
+		System.out.println(array3);
+
+		//Creating result file
+		File file = new File("./src/test/resources/results/difference.csv");
+		FileWriter fw = new FileWriter(file);
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write("Source,"+"Target,"+"Difference,"+"Source,"+"Target,"+"Difference,"+"Source,"+"Target,"+"Difference,");
+		bw.newLine();
+
+		for(int i=3;i<array1.size();i++)
+		{
+			bw.write(array1.get(i)+","+array2.get(i)+","+array3.get(i)+",");
+			bw.write(array1.get(i+1)+","+array2.get(i+1)+","+array3.get(i+1)+",");
+			bw.write(array1.get(i+2)+","+array2.get(i+2)+","+array3.get(i+2)+",");
+			i=i+2;
+			bw.newLine();
+		}
+
+		bw.close();
+		fw.close();
+	}
+
+}
